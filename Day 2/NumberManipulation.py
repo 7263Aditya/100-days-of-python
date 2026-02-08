ae429c8e6585892a04d1d9fadf5ff203d2bdb59f
Aditya Ghabade <gghabade@gmail.com>
2026-02-08 19:30:45 +0530

    Day 2 fixes

---
+++ Day 2/NumberManipulation.py
@@ -0,0 +1,23 @@
+bmi = 105 / 1.67 ** 2
+print(bmi)
+
+print(int(bmi))
+
+print(round(bmi))
+print(round(bmi, 2))
+
+score = 0
+
+# User scores a point
+score += 1
+print(score)
+
+# f-strings
+print("Your score is " + str(score))
+
+
+score = 0
+height = 1.8
+is_winning =True
+
+print(f"your score is =  {score}, your height is = {height}, you are winning is {is_winning}")
---
+++ Day 2/TypeErrorErrorCheckingConversion.py
@@ -0,0 +1,17 @@
+print(len("12345"))
+
+print(type("Hello"))
+print(type(1234))
+print(type(12.34))
+print(type(True))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
+
+
+username = input("Enter your name:")
+len_username = len(username)
+
+print(type("number of letter in your username: ")) # str
+print(type(len_username)) # int
+
+print("Number of letters in your username: " + str(len_username))
