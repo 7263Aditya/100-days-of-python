f2d5ffd2aa8bdc2d47b3769febf45ff16032b21a
Aditya Ghabade <gghabade@gmail.com>
2026-02-12 22:49:42 +0530

    Day 8 - Caeser Cipher

---
+++ Day 8/FunctionsWithInputs.py
@@ -0,0 +1,16 @@
+def greet():
+    print("Hello")
+    print("How do you do?")
+    print("Isn't that a great weather?")
+greet()
+greet()
+
+# Function that allows for inputs
+
+# Functions with input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+greet_with_name("Aditya")
