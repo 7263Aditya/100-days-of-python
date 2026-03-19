27c922064382b4b79a28469d7f5f83b48b5eca9c
Aditya Ghabade <gghabade@gmail.com>
2026-03-19 15:31:03 +0530

    Day 26 - NATO Alphabet Project using Dict Comprehension 🔠

--- Day 26/dict_comprehension.py
+++ Day 26/dict_comprehension.py
@@ -1,6 +1,5 @@
 # Dictionary Comprehension
-
-new_dict = [new_key:new_value for item in list]
+new_dict = {new_key:new_value for item in list}
 
 # Conditional dictionary comprehension
 new_dict = {new_key:new_value for (key,value) in dict.items() if test}
