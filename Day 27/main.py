d4e3fad75d118d226fdd8d7a23d67c157f87b453
Aditya Ghabade <gghabade@gmail.com>
2026-03-22 23:19:40 +0530

    Day 28: Completed Pomodoro Project with Canvas & Timer logic 🍅⏱️

--- Day 27/main.py
+++ Day 27/main.py
@@ -14,15 +14,12 @@ my_label.config(text = "New Text")
 my_label.grid(column=0,row=0)
 my_label.config(padx=50,pady=50)
 
-
 # Button
 def button_clicked():
     print("I got clicked")
     new_text = input.get()
     my_label.config(text = new_text)
     print()
-
-
 button = tk.Button(text="Click me",command= button_clicked)
 button.grid(column=1,row=1)
 # button.pack()
@@ -32,7 +29,6 @@ new_button = tk.Button(text="New Button",command=button_clicked)
 new_button.grid(column=2,row=0)
 
 # Entry
-
 input = tk.Entry(width=40)
 # input.pack()
 input.grid(column=3,row=2)
