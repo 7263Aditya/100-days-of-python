ef44fb2deb44fcb7b14e5004571597e9e06c6081
Aditya Ghabade <gghabade@gmail.com>
2026-03-30 23:51:21 +0530

    Day 34: Quizzler App with Open Trivia API & Class-based Tkinter UI 🧠🛡️

--- Day 33/kanye_quotes/main.py
+++ Day 33/kanye_quotes/main.py
@@ -1,15 +1,12 @@
 from tkinter import *
 import requests
 
-
 def get_quote():
     response = requests.get("https://api.kanye.rest")
     response.raise_for_status()
     data = response.json()
     quote = data["quote"]
     canvas.itemconfig(quote_text, text=quote)
-
-
 
 window = Tk()
 window.title("Kanye Says...")
